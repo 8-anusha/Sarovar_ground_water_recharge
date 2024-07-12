e919cf86623b52a57e8e0c105263b8540295989c
8-anusha <156271857+8-anusha@users.noreply.github.com>
2024-07-12 07:50:34 +0530

    Add files via upload

---
+++ hackathon/index2.js
@@ -0,0 +1,15 @@
+const data = [
+    [
+        { x: 1, y: 10 },
+        { x: 2, y: 15 },
+        { x: 3, y: 8 },
+        // ...
+    ],
+    [
+        { x: 1, y: 5 },
+        { x: 2, y: 12 },
+        { x: 3, y: 9 },
+        // ...
+    ],
+    // Repeat for the other series
+];
